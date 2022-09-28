748c5f83f1ea0455d2e78d64314f1be5f41b82af
Adam De Rango <113118048+adamdera@users.noreply.github.com>
2022-09-28 09:52:47 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+//  Created by: Adam Derango
+//  
+//  Date: september 28
+//  
+//  This sets the hungryness level to 0 on start
 let hungryness = 0
-basic.forever(function () {
+//  Created by: Adam Derango
+//  
+//  Date: september 28
+//  
+//  This adds 1 to the hungryness each time you press A 
+//  and resets the counter to zero each time you press B
+basic.forever(function on_forever() {
+    
     if (input.buttonIsPressed(Button.A)) {
         hungryness = hungryness + 1
         basic.showNumber(hungryness)
@@ -7,4 +19,5 @@ basic.forever(function () {
         hungryness = 0
         basic.showNumber(hungryness)
     }
+    
 })
